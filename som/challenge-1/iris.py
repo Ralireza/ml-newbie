aa4f896deb9eb9c653e6e0daa9bbcc7385099c05
4lrz <alirezarezaie0101@gmail.com>
2019-05-31 16:13:16 +0430

    add som iris challenge

---
+++ som/challenge-1/iris.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from som.SelfOrganizedMap import SOM
+
+
+# https://tcosmo.github.io/2017/07/27/fun-with-som.html
+
+iris = datasets.load_iris()
+X = iris.data[:, :]
+y = iris.target
+np.random.seed(1)
+weight = 0.01
+indices = np.random.permutation(len(X))
+data = X[indices[:]]
+labels = X[indices[:]]
+
+som_iris = SOM(20, 20, 4)
+frames_iris = []
+som_iris.train(data, L0=0.8, lam=1e2, sigma0=10, frames=frames_iris)
+print("quantization error:", som_iris.quant_err())
